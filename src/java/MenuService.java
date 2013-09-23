4c7c3627afe488e57a9dbbabba49b6a6f524cdf6
Andy <Andy@Andy-Desktop>
2013-09-23 15:11:38 -0500

    MenuService class

--- src/java/MenuService.java
+++ src/java/MenuService.java
@@ -1,3 +1,6 @@
+
+import java.util.List;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -8,5 +11,29 @@
  * @author Andy
  */
 public class MenuService {
+    
+    private I_MenuDAO menuDAO;
+
+    public MenuService() {
+        I_DBAccessor db = new DBGenericAccessor();
+        menuDAO = new MenuDAO(db);
+    }
+    
+    public List<Menu> getAllMenuItems() throws DataAccessException{
+        return menuDAO.retrieveAllMenuItems();
+    }
+    
+    
+    public static void main(String[] args) throws DataAccessException {
+        MenuService ms = new MenuService();
+        
+        System.out.println("Menu Items: \n");
+        System.out.println(ms.getAllMenuItems());
+        
+    }
+    
+    
+    
+    
     
 }
